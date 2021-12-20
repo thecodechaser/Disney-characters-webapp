ae49776191672859015a27dcde33277fe7347b38
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2021-12-20 21:07:48 +0530

    Add elements to header

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,16 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+        },
+      },
     ],
   },
 };
