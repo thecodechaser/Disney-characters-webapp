c15bb419cfc6af05dbc17bb5860c14433b81f361
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2021-12-21 12:12:28 +0530

    Add html structure for cards

--- webpack.config.js
+++ webpack.config.js
@@ -27,14 +27,8 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
         test: /\.html$/i,
         loader: "html-loader",
-        options: {
-        },
       },
     ],
   },
