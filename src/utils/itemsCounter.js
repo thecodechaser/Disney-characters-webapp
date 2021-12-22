595a653d6e32602d015f11439ad8685bd6d67702
Roshan Bajgain <you@example.com>
2021-12-23 00:10:54 +0545

    Update dist directory

--- src/utils/itemsCounter.js
+++ src/utils/itemsCounter.js
@@ -1,7 +1,7 @@
 import { getCharData } from './retrieveApis.js';
 
 export default async () => {
-    const itemsCounter = await getCharData();
-    const characters = document.querySelector('.c-counter');
-    characters.innerHTML = `Characters(${itemsCounter.length})`
+  const itemsCounter = await getCharData();
+  const characters = document.querySelector('.c-counter');
+  characters.innerHTML = `Characters(${itemsCounter.length})`;
 };
