d0839e85ffb2384096dc4940b93dcb493de22cb2
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2021-12-23 15:54:47 +0530

    Add test for comment counter

--- src/_mocks_/commentsCounter.js
+++ src/_mocks_/commentsCounter.js
@@ -1,7 +1,7 @@
 import fetch from 'cross-fetch';
 
 export default async (dataID) => {
-  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/byg3KtvqOhmd3Xt9Axu5/comments?item_id=item${dataID}');
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/byg3KtvqOhmd3Xt9Axu5/comments?item_id=item${dataID}`);
   const comments = await response.json();
   return comments;
 };
