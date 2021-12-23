3bf2f26c21d10f92d34f8e64fb502b4fc9cee364
Roshan Bajgain <you@example.com>
2021-12-23 14:25:29 +0545

    Update dist directory

--- src/_mocks_/itemsCounter.js
+++ src/_mocks_/itemsCounter.js
@@ -1,7 +1,7 @@
 import fetch from 'cross-fetch';
 
 export default async () => {
-    const response = await fetch('https://api.disneyapi.dev/characters');
+  const response = await fetch('https://api.disneyapi.dev/characters');
   const json = await response.json();
   return json.data;
-}
+};
--- src/_tests_/itemsCounter.test.js
+++ src/_tests_/itemsCounter.test.js
@@ -1,28 +1,26 @@
-import itemsCounter from '../_mocks_/itemsCounter';
+import itemsCounter from '../_mocks_/itemsCounter.js';
 
 describe('Check Items count number', () => {
-
-    test('if the function actually does the count', async () => {
-        const data = await itemsCounter();
-        const count = data.length;
-      expect(count).toEqual(50);
-
-    });
-
-    test('if the function actually does the count', async () => {
-        const data = await itemsCounter();
-        const count = data.length;
-        expect(count === data.length).toBe(true);
-      });
-
-      test('if the function actually does the count', async () => {
-        const data = await itemsCounter();
-        const count = data.length;
-        expect(typeof count).toEqual('number');
-      });
-
-      test('if the function actually does the count', async () => {
-        const data = await itemsCounter();
-        expect(typeof data).toEqual('object');
-      });
+  test('if the function actually does the count', async () => {
+    const data = await itemsCounter();
+    const count = data.length;
+    expect(count).toEqual(50);
   });
+
+  test('if the function actually does the count', async () => {
+    const data = await itemsCounter();
+    const count = data.length;
+    expect(count === data.length).toBe(true);
+  });
+
+  test('if the function actually does the count', async () => {
+    const data = await itemsCounter();
+    const count = data.length;
+    expect(typeof count).toEqual('number');
+  });
+
+  test('if the function actually does the count', async () => {
+    const data = await itemsCounter();
+    expect(typeof data).toEqual('object');
+  });
+});
